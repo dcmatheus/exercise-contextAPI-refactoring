0ef8c5c102cf5f541ee301d7809f76ed68d33dc8
Matheus Costa <mathws.costag@gmail.com>
2021-07-21 17:46:17 -0300

    Troca Redux por Context API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './myContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+const initialState = {
+  red: false,
+  blue: false,
+  yellow: false,
+};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = initialState;
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(key, boo) {
+    this.setState({ [key]: boo });
+  }
+
+  render() {
+    const { red, blue, yellow } = this.state;
+    const value = {
+      red,
+      blue,
+      yellow,
+      moveCar: this.moveCar,
+    }
+    return (
+      <MyContext.Provider value={value}>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
+
 
 export default App;
