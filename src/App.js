9b335ccd98abbc57af292f2197421982f425c0d2
Matheus Costa <mathws.costag@gmail.com>
2021-07-21 18:22:07 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import TrafficSignal from './TrafficSignal';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './myContext';
 
-function App() {
-  return (
-    <div className="container">
-      <Cars />
-      <TrafficSignal />
-    </div>
-  );
+const initialState = {
+  redCar: false,
+  blueCar: false,
+  yellowCar: false,
+};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = initialState;
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(key, boo) {
+    this.setState({ [key]: boo });
+  }
+
+  render() {
+    const { red, blue, yellow } = this.state;
+    const value = {
+      red,
+      blue,
+      yellow,
+      moveCar: this.moveCar,
+    }
+    return (
+      <MyContext.Provider value={value}>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
+
 
 export default App;
